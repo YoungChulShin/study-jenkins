600ba7107632d03fff96161656e34a4ac70f69fe
YoungChulShin <go1323@gmail.com>
2023-12-13 23:17:07 +0900

    simple project 추가

---
+++ simple-project/src/test/java/study/jenkins/simpleproject/SimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jenkins.simpleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
